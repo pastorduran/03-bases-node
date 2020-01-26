c4815fab523db13933b54580f0f4afadd31c22ee
Pastor Durán <pastorjduran@gmail.com>
2020-01-26 16:40:51 -0300

    commit inicial tabla multiplicar

--- app.js
+++ app.js
@@ -15,21 +15,7 @@ const { crearArchivo } = require('./multiplicar/multiplicar');
 
 const { listarTabla } = require('./multiplicar/multiplicar');
 
-/*
-let data = '';
-
-for (let i = 1; i < 10; i++) {
-    //console.log(`${base} * ${i} = ${base * i}`);
-    data += `${base} * ${i} = ${base * i}\n`;
-}
-
-fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
-    if (err) throw err;
-    console.log(`El Archivo tabla-${base}.txt ha sido creado`);
-}); */
-
-//console.log(argv);
-
+//Tomo la lista de comandos ingresados en consola
 let comando = argv._[0];
 
 switch (comando) {
@@ -45,11 +31,3 @@ switch (comando) {
     default:
         console.log('Comando no reconocido');
 }
-
-//console.log('Limite ', argv.limite);
-
-//let parametro = argv[2]
-
-//let base = parametro.split('=')[1];
-
-//console.log(base);
